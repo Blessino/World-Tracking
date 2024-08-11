2a8948bfbe9c0e5f236c0a19fbd7d9b1de0f3504
Ashish <grunch.me1@gmail.com>
2024-08-11 12:51:40 +0545

    dynamic routing lat lng is made

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,16 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map() {
+  const [searchParms, setSearchParams] = useSearchParams();
+  const lat = searchParms.get("lat");
+  const lng = searchParms.get("lng");
   return (
     <div className={styles.mapContainer}>
         <p>Map</p>
+        <h1>
+          position: {lat}, {lng}
+        </h1>
     </div>
   )
 }
